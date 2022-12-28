48ee9589bd2827bda021983e325e0dff83931b2f
Francisco Garcia <jjjchico1@gmail.com>
2022-12-28 16:20:00 -0600

    database connection with typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from './modules/user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'chico007321',
+      database: 'tasks',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+
+    UserModule],
   controllers: [],
   providers: [],
 })
