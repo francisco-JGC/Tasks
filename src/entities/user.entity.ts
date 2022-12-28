d826299e1c0d17e3bfd09c0d1e3653f857ea87c4
Francisco Garcia <jjjchico1@gmail.com>
2022-12-28 16:24:27 -0600

    created entity for user

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ unique: true, nullable: false })
+    username: string;
+
+    @Column( { unique: true, nullable: false } )
+    email: string;
+
+    @Column({ select: false, nullable: false})
+    password: string;
+}
