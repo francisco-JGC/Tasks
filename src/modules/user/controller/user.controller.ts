6631b9b356d9f21cb73e19a16fff94d57eb1436e
Francisco Garcia <jjjchico1@gmail.com>
2022-12-28 20:18:12 -0600

    add new functions to controller and service (find, delete, update)

--- src/modules/user/controller/user.controller.ts
+++ src/modules/user/controller/user.controller.ts
@@ -16,4 +16,35 @@ export class UserController {
 
         return await this.userService.createUser(userObject);
     }
+
+    @Get('find/:id')
+    async findOne(@Param('id') id: number) {
+        return await this.userService.findUserById(id);
+    }
+ 
+    @Get('find/username/:username')
+    async findOneByUsername(@Param('username') username: string) {
+        return await this.userService.findUserByUsername(username);
+    }
+
+    @Get('find/email/:email')
+    async findOneByEmail(@Param('email') email: string) {
+        return await this.userService.findUserByEmail(email);
+    }
+
+    @Patch('update/:id')
+    async update(@Param('id') id: number, @Body() userObject: any) {
+        // check if body is empty
+        if (Object.keys(userObject).length === 0) throw new HttpException('Body cannot be empty', 400);
+
+        // check if the password is valid length
+        if (userObject.password.length < 4) throw new HttpException('Password must be at least 4 characters', 400);
+
+        return await this.userService.updateUser(id, userObject);
+    }
+
+    @Delete('delete/:id')
+    async remove(@Param('id') id: number) {
+        return await this.userService.deleteUser(id);
+    }
 }
--- src/modules/user/service/user.service.ts
+++ src/modules/user/service/user.service.ts
@@ -32,22 +32,45 @@ export class UserService {
         return await this.userRepo.save(user) as User;
     }
 
-    async findUserById( id: number ): Promise<User> {
+    async findUserById( id: number ): Promise<User | {}> {
         // findOneBy is a custom method
         const user = await this.userRepo.findOneBy({ id }); 
-        return user ? user : null;
+        return user ? user : { statusCode: 404, message: 'User not found', };
     }
 
-    async findUserByUsername( username: string ): Promise<User> {
+    async findUserByUsername( username: string ): Promise<User | {}> {
         // findOneBy is a custom method
         const user = await this.userRepo.findOneBy({ username });
-        return user ? user : null;
+        return user ? user : { statusCode: 404, message: 'User not found', };
     }
 
-    async findUserByEmail( email: string ): Promise<User> {
+    async findUserByEmail( email: string ): Promise<User | {}> {
         // findOneBy is a custom method
         const user = await this.userRepo.findOneBy({ email });
-        return user ? user : null;
+        return user ? user : { statusCode: 404, message: 'User not found', };
+    }
+
+    async updateUser( id: number, userObject: RegisterUserDto ): Promise<User> {
+        // check if the user already exists
+        const userExists = await this.userRepo.findOneBy({ id });
+        if (!userExists) throw new HttpException('User does not exist', 400);
+        
+        // merge the user object with the user object from the database
+        const user = this.userRepo.merge(userExists, userObject);
+
+        // save the user object to the database
+        return await this.userRepo.save(user) as User;
+    }
+
+    async deleteUser( id: number ): Promise<{}> {
+        // check if the user already exists
+        const userExists = await this.userRepo.findOneBy({ id });
+        if (!userExists) throw new HttpException('User does not exist', 400);
+
+        // delete the user object from the database
+        await this.userRepo.remove(userExists);
+
+        return { statusCode: 200, message: 'User deleted', };
     }
     
 }
