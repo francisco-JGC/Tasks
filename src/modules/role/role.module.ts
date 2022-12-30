9174f4396fb01ec4046b96a524687b31c3ba8300
Francisco Garcia <jjjchico1@gmail.com>
2022-12-30 17:38:51 -0600

    import of service and user entity

--- src/modules/role/role.module.ts
+++ src/modules/role/role.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { RoleService } from './service/role.service';
 import { RoleController } from './controller/role.controller';
+import { Role } from 'src/entities/role.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Role, User])],
   controllers: [RoleController],
-  providers: [RoleService]
+  providers: [RoleService],
+  exports: [RoleService]
 })
 export class RoleModule {}
