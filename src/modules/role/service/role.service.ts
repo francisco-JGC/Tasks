e9da9a05195dccfc073d70d857c3f14c6c59c8db
Francisco Garcia <jjjchico1@gmail.com>
2022-12-30 17:28:23 -0600

    role service, new functions for role management and init function to initialize main roles

--- src/modules/role/service/role.service.ts
+++ src/modules/role/service/role.service.ts
@@ -1,26 +1,61 @@
 import { Injectable } from '@nestjs/common';
 import { CreateRoleDto } from '../dto/create-role.dto';
-import { UpdateRoleDto } from '../dto/update-role.dto';
+import { Role } from 'src/entities/role.entity';
+import { User } from 'src/entities/user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { HttpException } from '@nestjs/common/exceptions';
 
 @Injectable()
 export class RoleService {
-  create(createRoleDto: CreateRoleDto) {
-    return 'This action adds a new role';
-  }
+    constructor(
+        @InjectRepository(Role) private readonly roleRepo: Repository<Role>,
+        @InjectRepository(User) private readonly userRepo: Repository<User>,
+    ) {}
+    
+    async init() : Promise<void> {
+        const role = await this.roleRepo.findOneBy({ name: 'user' });
+        if (!role) {
+            const newRole = this.roleRepo.create({ name: 'user' });
+            await this.roleRepo.save(newRole);
+        }
 
-  findAll() {
-    return `This action returns all role`;
-  }
+        const adminRole = await this.roleRepo.findOneBy({ name: 'admin' });
+        if (!adminRole) {
+            const newRole = this.roleRepo.create({ name: 'admin' });
+            await this.roleRepo.save(newRole);
+        }
 
-  findOne(id: number) {
-    return `This action returns a #${id} role`;
-  }
+        return;
+    }
 
-  update(id: number, updateRoleDto: UpdateRoleDto) {
-    return `This action updates a #${id} role`;
-  }
+    async create(createRoleDto: CreateRoleDto): Promise<Role> {
+        const { name } = createRoleDto;
 
-  remove(id: number) {
-    return `This action removes a #${id} role`;
-  }
+        // check if role is empty
+        if (!name) throw new HttpException('Role is empty', 400);
+
+        // check if role already exists
+        const roleExists = await this.roleRepo.findOneBy({ name});
+        if (roleExists) throw new HttpException('Role already exists', 400);
+
+        const role = this.roleRepo.create(createRoleDto);
+        return await this.roleRepo.save(role);
+    }
+
+
+    async getDefaultRole(): Promise<Role> {
+        return await this.roleRepo.findOneBy({ name: 'user' });
+    }
+
+    async removeRoleFromUser(userId: number, roleId: number): Promise<User> {
+        const user = await this.userRepo.findOneBy({ id: userId });
+        const role = await this.roleRepo.findOneBy({ id: roleId });
+
+        if (!user) throw new HttpException('User does not exist', 400);
+        if (!role) throw new HttpException('Role does not exist', 400);
+
+        user.roles = user.roles.filter(r => r.id !== role.id);
+        return await this.userRepo.save(user);
+    }
 }
