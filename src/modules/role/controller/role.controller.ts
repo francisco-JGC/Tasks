5713de96587d57503cc446aaa282a536aa278c9b
Francisco Garcia <jjjchico1@gmail.com>
2023-01-03 13:18:42 -0600

    new functionality for the controller for the functions of adding and removing user roles

--- src/modules/role/controller/role.controller.ts
+++ src/modules/role/controller/role.controller.ts
@@ -6,4 +6,15 @@ import { CreateRoleDto } from '../dto/create-role.dto';
 export class RoleController {
   constructor(private readonly roleService: RoleService) {}
 
+  @Post('assign-role-to-user')
+  async assignRoleToUser(@Body() body: any) {
+    const { userId, roleId } = body;
+    return await this.roleService.assignRoleToUser(userId, roleId);
+  }
+
+  @Post('remove-role-from-user')
+  async removeRoleFromUser(@Body() body: any) {
+    const { userId, roleId } = body;
+    return await this.roleService.removeRoleFromUser(userId, roleId);
+  }
 }
