bce660a835107033411f51b8f0ea7f5bf72d1566
Francisco Garcia <jjjchico1@gmail.com>
2023-01-03 13:06:09 -0600

    import provider JwtService

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -4,10 +4,11 @@ import { UserController } from './controller/user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
 import { JwtStrategy } from '../auth/jwt.strategy';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
-  providers: [UserService, JwtStrategy]
+  providers: [UserService, JwtStrategy, JwtService]
 })
 export class UserModule {}
